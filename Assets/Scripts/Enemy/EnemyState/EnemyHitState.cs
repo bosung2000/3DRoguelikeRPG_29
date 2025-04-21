ad081f57c09e98faebc696333bb2d79e3e187def
321213132 <minhun5257@gmail.com>
2025-04-21 20:35:28 +0900

    [feat] 피격 시 공격 쿨 초기화

--- Assets/Scripts/Enemy/EnemyState/EnemyHitState.cs
+++ Assets/Scripts/Enemy/EnemyState/EnemyHitState.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +12,8 @@ public class EnemyHitState : IEnemyState
         _timer = 0f;
         controller.animator.SetTrigger("Hit");
         controller.agent.enabled = true;
+
+        controller.ResetAttackCooldown();
     }
 
     public void ExitState(EnemyController controller)
