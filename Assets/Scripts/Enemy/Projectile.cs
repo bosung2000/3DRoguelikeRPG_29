0b0b92da4733fb37dc8ffef960c5b773b3133eea
321213132 <minhun5257@gmail.com>
2025-04-23 11:25:35 +0900

    [feat]투사체 발사 기능 구현

---
+++ Assets/Scripts/Enemy/Projectile.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float _speed = 10f; //투사체 속도 설정
+    [SerializeField] private int _damage;         //데이터에서 가져오기
+
+    private Vector3 _direction;
+
+    private void FixedUpdate()
+    {
+        transform.position += _direction * _speed * Time.deltaTime;
+    }
+
+    public void Intialize(Vector3 dir, int dmg)
+    {
+        _direction = dir.normalized;
+        _damage = dmg;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Enemy"))
+        {
+            return;
+        }
+
+        if(other.CompareTag("Player"))
+        {
+            PlayerStat player = other.GetComponent<PlayerStat>();
+            if(player != null)
+            {
+                player.TakeDamage(_damage);
+            }
+        }
+        
+        Destroy(gameObject);
+        return;
+    }
+}
