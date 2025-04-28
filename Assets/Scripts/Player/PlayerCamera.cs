6e7865383b3602da38be62a280feb497ad163d67
hanbin125 <cyhm2545@naver.com>
2025-04-28 15:42:51 +0900

    [Feat] 카메라 화면밖으로 벗어나는거추가

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class PlayerCamera : MonoBehaviour
 {
     [SerializeField] private Player player;
-    [SerializeField] private Vector3 offset = new Vector3(0, 7.5f, -10);
+    [SerializeField] private Vector3 offset = new Vector3(0, 7.5f, -4);
     [SerializeField] private Vector3 Rotation = new Vector3(60, 0, 0);
-    [SerializeField] private float followSpeed = 0;
+    [SerializeField] private float followSpeed = 5;
 
     private void LateUpdate()
     {
