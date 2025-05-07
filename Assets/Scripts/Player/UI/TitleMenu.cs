5d6c243e99a7bd93c13afbbb5c6de2ec5302e1b3
hanbin125 <cyhm2545@naver.com>
2025-05-07 21:10:02 +0900

    [Feat] 로비씬과 게임씬 설정오류수정중

--- Assets/Scripts/Player/UI/TitleMenu.cs
+++ Assets/Scripts/Player/UI/TitleMenu.cs
@@ -3,26 +3,28 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-/// <summary>
-/// 타이틀 메뉴 및 씬 전환
-/// </summary>
+/// <summary>  
+/// 타이틀 메뉴 및 씬 전환  
+/// </summary>  
 public class TitleMenu : MonoBehaviour
 {
     [SerializeField] Button startBtn;
     [SerializeField] Button endBtn;
+    [SerializeField] Button soundBtn;
     [SerializeField] Button settingBtn;
-    //[SerializeField] Button closeSetting;
 
+    [SerializeField] GameObject soundMenu;
     [SerializeField] GameObject settingMenu;
     bool active;
 
     private void Start()
     {
-        startBtn.onClick.AddListener(OnClickStart);
+        if (startBtn != null) startBtn.onClick.AddListener(OnClickStart);
         endBtn.onClick.AddListener(OnClickEndBtn);
         settingBtn.onClick.AddListener(OnClickSettingBtn);
-        //closeSetting.onClick.AddListener(OnClickCloseSetting);
+        soundBtn.onClick.AddListener(OnClickSoundBtn);
     }
+
     public void OnClickStart()
     {
         SceneManager.LoadScene("Main_HB");
@@ -30,10 +32,27 @@ public class TitleMenu : MonoBehaviour
 
     public void OnClickEndBtn()
     {
+#if UNITY_EDITOR
         EditorApplication.isPlaying = false;
-
-        Application.Quit();
+#else
+       Application.Quit();  
+#endif
     }
+
+    public void OnClickSoundBtn()
+    {
+        if (active)
+        {
+            soundMenu.SetActive(false);
+            active = false;
+        }
+        else
+        {
+            soundMenu.SetActive(true);
+            active = true;
+        }
+    }
+
     public void OnClickSettingBtn()
     {
         if (active)
@@ -47,8 +66,4 @@ public class TitleMenu : MonoBehaviour
             active = true;
         }
     }
-    //public void OnClickCloseSetting()
-    //{
-    //    settingMenu.SetActive(false);
-    //}
 }
