59ccdc18a4fc30a8faff9ee15d76798720af6be5
Bosung <gobosung2000@gmail.com>
2025-05-08 12:12:41 +0900

    [Feat] 맵시스템 중간정검 (몬스터 생성까지는 완료)

--- Assets/Scripts/Map/RoomZone.cs
+++ Assets/Scripts/Map/RoomZone.cs
@@ -12,9 +12,20 @@ public class RoomZone : MonoBehaviour
 
     private List<Enemy> spawnedEnemies;
 
-    private void Start()
+    private void Awake()
     {
         spawnedEnemies = new();
+        
+        // 방 이름이 설정되지 않았으면 게임 오브젝트 이름을 사용
+        if (string.IsNullOrEmpty(roomName))
+        {
+            roomName = gameObject.name;
+            Debug.Log($"[RoomZone] 방 이름이 자동 설정됨: {roomName}");
+        }
+    }
+
+    private void Start()
+    {
     }
 
     public void ActivateRoom()
@@ -47,10 +58,10 @@ public class RoomZone : MonoBehaviour
             SpawnEnemy(spawnConfig.bossEnemyPrefab, point.position);
         }
 
-        if (spawnedEnemies.Count == 0 && nextRoom != null)
-        {
-            nextRoom.ActivateRoom();
-        }
+        //if (spawnedEnemies.Count == 0 && nextRoom != null)
+        //{
+        //    nextRoom.ActivateRoom();
+        //}
     }
 
     private void SpawnEnemy(GameObject prefab, Vector3 position)
