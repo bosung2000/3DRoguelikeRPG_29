d9d7ec55365773e1850be1f197a3cf035c245032
hanbin125 <cyhm2545@naver.com>
2025-05-08 20:36:23 +0900

    [Feat] UI오류수정

---
+++ Assets/Scripts/Player/CurrencySaveData.cs
@@ -0,0 +1,12 @@
+[System.Serializable]
+public class CurrencySaveData
+{
+    public int gold;
+    public int soul;
+
+    public CurrencySaveData(int gold, int soul)
+    {
+        this.gold = gold;
+        this.soul = soul;
+    }
+}
