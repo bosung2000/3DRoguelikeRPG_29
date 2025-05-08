185f0bc64932ac159530d3f083e1fb8d35bbae27
Bosung <gobosung2000@gmail.com>
2025-05-08 20:18:39 +0900

    [Feat] playerStat 피드백 받기전

--- Assets/Scripts/Player/BaseStat.cs
+++ Assets/Scripts/Player/BaseStat.cs
@@ -48,32 +48,32 @@ public interface IBaseStat<T>
 
 public abstract class BaseStat<T> : MonoBehaviour, IBaseStat<T> where T : Enum
 {
-    protected Dictionary<T, float> stats = new Dictionary<T, float>();
+    protected Dictionary<T, float> Basestats = new Dictionary<T, float>();
 
     protected virtual void InitializeStats()
     {
         foreach (T type in Enum.GetValues(typeof(T)))
         {
-            stats[type] = 0f;
+            Basestats[type] = 0f;
         }
     }
 
     public virtual float GetStatValue(T type)
     {
-        return stats.TryGetValue(type, out float value) ? value : 0f;
+        return Basestats.TryGetValue(type, out float value) ? value : 0f;
     }
 
     public virtual void SetStatValue(T type, float value)
     {
-        stats[type] = value;
+        Basestats[type] = value;
         OnStatChanged();
     }
 
     public virtual void ModifyStat(T type, float amount)
     {
-        if (stats.ContainsKey(type))
+        if (Basestats.ContainsKey(type))
         {
-            stats[type] += amount;
+            Basestats[type] += amount;
             OnStatChanged();
         }
     }
