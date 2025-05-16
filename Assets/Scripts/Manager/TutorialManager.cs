f150db8d8477975da6dabb0c8b0cc1656b7f54e0
정구 <kinggodjung9@naver.com>
2025-05-16 11:21:49 +0900

    [chore[ 중간저장

--- Assets/Scripts/Manager/TutorialManager.cs
+++ Assets/Scripts/Manager/TutorialManager.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TutorialManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject[] tutorialSteps;
+    private int currentStepIndex = 0;
+
     void Start()
     {
-        
+        //첫 단계만 활성화
+        for (int i = 0; i < tutorialSteps.Length; i++)
+        {
+            tutorialSteps[i].SetActive(i == 0);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    //다음 단계
+    public void NextStep()
     {
-        
+        if (currentStepIndex < tutorialSteps.Length - 1)
+        {
+            tutorialSteps[currentStepIndex].SetActive(false);
+            currentStepIndex++;
+            tutorialSteps[currentStepIndex].SetActive(true);
+        }
+        else
+        {
+            Debug.Log("튜토리얼 완료");
+            tutorialSteps[currentStepIndex].SetActive(false);
+        }
     }
 }
