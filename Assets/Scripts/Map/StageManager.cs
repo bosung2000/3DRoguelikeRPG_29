0b4c9c2713d7ddd323868a5c60f2323328836cd3
Bosung <gobosung2000@gmail.com>
2025-05-20 19:46:04 +0900

    [Feat] 밸패_02 >몬스터 공+이속 스테이지별 증가 +흡혈 너프

--- Assets/Scripts/Map/StageManager.cs
+++ Assets/Scripts/Map/StageManager.cs
@@ -6,6 +6,8 @@ public class StageManager : MonoBehaviour
     private int MaxStage = 5;
     public int CurrentStage { get; private set; } = 1;
     public float HpMultiplier => 1f + 0.3f * (CurrentStage - 1);
+    public float AttackMultiplier => 1f + 0.3f * (CurrentStage - 1);
+    public float SpeedMultiplier => 0.5f;
 
     private void Awake()
     {
