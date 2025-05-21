1192037a3fb12c653cadbc34dcbe0fdebd7e43c8
정구 <kinggodjung9@naver.com>
2025-05-21 19:25:46 +0900

    [feat] 데이터 저장 경로 추가

--- Assets/Scripts/Manager/SaveManager.cs
+++ Assets/Scripts/Manager/SaveManager.cs
@@ -3,29 +3,43 @@ using System.IO;
 
 public class SaveManager : MonoBehaviour
 {
+#if !UNITY_WEBGL
     private string savePath;
+#endif
 
     private void Awake()
     {
+#if !UNITY_WEBGL
         savePath = Path.Combine(Application.persistentDataPath, "save.json");
-
+#endif
         DontDestroyOnLoad(this.gameObject);
     }
 
     public void SaveData(GameData data)
     {
-        if (string.IsNullOrEmpty(savePath))
-        {
-            savePath = Path.Combine(Application.persistentDataPath, "save.json");
-        }
-
         string json = JsonUtility.ToJson(data, true);
+
+#if UNITY_WEBGL
+        PlayerPrefs.SetString("GameData", json);
+        PlayerPrefs.Save();
+#else
         File.WriteAllText(savePath, json);
-        Debug.Log($"저장 완료: {savePath}");
+#endif
     }
 
     public GameData LoadData()
     {
+#if UNITY_WEBGL
+        if (PlayerPrefs.HasKey("GameData"))
+        {
+            string json = PlayerPrefs.GetString("GameData");
+            return JsonUtility.FromJson<GameData>(json);
+        }
+        else
+        {
+            return new GameData(); // 초기값 리턴
+        }
+#else
         if (File.Exists(savePath))
         {
             string json = File.ReadAllText(savePath);
@@ -33,8 +47,20 @@ public class SaveManager : MonoBehaviour
         }
         else
         {
-            Debug.Log("저장 파일 없음. 새로 생성");
-            return new GameData();        
+            return new GameData(); // 초기값 리턴
         }
+#endif
+    }
+
+    public void ClearData()
+    {
+#if UNITY_WEBGL
+        PlayerPrefs.DeleteKey("GameData");
+#else
+        if (File.Exists(savePath))
+        {
+            File.Delete(savePath);
+        }
+#endif
     }
 }
