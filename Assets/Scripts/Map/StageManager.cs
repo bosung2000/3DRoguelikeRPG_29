15d9417c27dfd4bb8b08306cb421d7ff219c87a0
Bosung <gobosung2000@gmail.com>
2025-05-21 11:03:57 +0900

    [Feat] 밸패 _01

--- Assets/Scripts/Map/StageManager.cs
+++ Assets/Scripts/Map/StageManager.cs
@@ -6,7 +6,7 @@ public class StageManager : MonoBehaviour
     private int MaxStage = 5;
     public int CurrentStage { get; private set; } = 1;
     public float HpMultiplier => 1f + 0.7f * (CurrentStage - 1);
-    public float AttackMultiplier => 1f + 0.3f * (CurrentStage - 1);
+    public float AttackMultiplier => 1f + 1f * (CurrentStage - 1);
     public float SpeedMultiplier => 0.5f *(CurrentStage - 1);
 
     private void Awake()
